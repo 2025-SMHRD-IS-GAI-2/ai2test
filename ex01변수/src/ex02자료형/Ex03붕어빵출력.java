4724aefd6729dd7922357848e4e515e2634685ca
smhrd <smhrd@192.168.219.65>
2025-10-23 10:22:28 +0900

    첫 커밋 완료! -> 특정 시점의 스냅샷

---
+++ ex01변수/src/ex02자료형/Ex03붕어빵출력.java
@@ -0,0 +1,24 @@
+package ex02자료형;
+
+import java.util.Scanner;
+
+public class Ex03붕어빵출력 {
+
+	public static void main(String[] args) {
+        // 1. 입력 받는 도구 가져오기
+		Scanner sc = new Scanner(System.in);
+		// 2. "구매 희망하는 팥붕 개수" 출력
+		System.out.println("구매 희망하는 팥붕개수");
+		// 3. 팥붕 개수 입력받아서 변수 num에 저장
+		int num =sc.nextInt();
+		//    -> 팥붕 개수 입력받아, 변수 num선언하여 할당!
+		// 4. num 출력!
+		System.out.println("붕어빵의 개수 :"+num);
+		
+		
+		
+		
+		
+	}
+
+}
